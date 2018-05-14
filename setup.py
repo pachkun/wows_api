a5f6537aff6b559e23ef754ddedd731702653002
pachkun <pachkunishka@gmail.com>
2018-05-14 23:48:01 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
-import wows_api
+
 
 setup(
     name='wows_api',
-    version=wows_api.__version__,
+    version='0.1',
     packages=['wows_api'],
     install_requires=['requests'],
     url='https://github.com/pachkun/wows_api',
     author='pachkun',
     author_email='pachkunishka@gmail.com',
     description='Работа с api world of warship',
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    include_package_data=True
 )
